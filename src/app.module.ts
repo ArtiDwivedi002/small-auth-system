71719cb78d5a85c3ee3b323db53cb82ee69b5a64
Arti <dwivediarti214@gmail.com>
2026-05-28 07:22:07 +0530

    changes some file

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,16 +21,22 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     // PostgreSQL Database Connection
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+   TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+}),
 
     // GraphQL Configuration
     GraphQLModule.forRoot<ApolloDriverConfig>({
